5e46237bf81c794df14ac30f348ab9f44088ec36
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2021-04-10 20:19:39 -0500

    login page added

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,4 @@
-"""project URL Configuration
-
+"""TAproject URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -15,7 +14,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from project_app.views import Login
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Login.as_view())
 ]
--- project_app/views.py
+++ project_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import User
 # Create your views here.
+class Login(View):
+    def get(self,request):
+        return render(request,"login.html",{})
+    def post(self,request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = User.objects.get(name=request.POST['name'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser:
+            return render(request,"login.html",{"message":"username does not exist"})
+        elif badPassword:
+            return render(request,"login.html",{"message":"incorrect password"})
+        else:
+            request.session["name"] = m.name
+            return redirect("/things/")
