5bfef926af3d51edfe4d62a34088b2e1d57c8f73
CharlieEllenbecker <57051469+CharlieEllenbecker@users.noreply.github.com>
2021-04-26 18:30:55 -0500

    updated the placeholders for the forms pages

--- project_app/models.py
+++ project_app/models.py
@@ -14,30 +14,17 @@ class User(models.Model):
     role = models.CharField(max_length=10, choices=Roles.choices)
     phone = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=50, blank=True)
-
-    # officehours may need to be a sort of datetime object
     officehours = models.CharField(max_length=20, blank=True)
 
 class Course(models.Model):
     courseid = models.CharField(max_length=20)
     coursename = models.CharField(max_length=50)
-
-    # courseschedule may need to be a sort of datetime object
     courseschedule = models.CharField(max_length=20, blank=True)
     coursecredits = models.CharField(max_length=2)
-
-    # courseinstructor = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
-
-    # courseTA may be a list including foreign key to the TA user, a graderstatus boolean, and # number of labs they are in
-    # courseTA = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    # graderstatus = models.BooleanField(default=False)
 
 class Lab(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     labid = models.CharField(max_length=20)
     labname = models.CharField(max_length=50)
-
-    # labschedule may need to be a sort of datetime object
     labschedule = models.CharField(max_length=20, blank=True)
-    # labTA = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
