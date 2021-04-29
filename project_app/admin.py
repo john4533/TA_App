dafbe2fe6574b0e742d6fb5c072adb6a76b0dc24
anton <langbru2@uwm.edu>
2021-04-29 12:18:40 -0500

    update to models

--- project_app/admin.py
+++ project_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from project_app.models import User, Course, Lab
+from project_app.models import User, Course, Section
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Course)
-admin.site.register(Lab)
+admin.site.register(Section)
