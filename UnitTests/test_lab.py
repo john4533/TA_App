56a1341e6a8783a5b23b7db4d33de5d3b6010592
kkkha <yk3245385@gamil.com>
2021-04-30 17:30:13 -0500

    Labs changed to sectons

--- UnitTests/test_lab.py
+++ UnitTests/test_lab.py
@@ -5,41 +5,41 @@ import django
 django.setup()
 from Classes.functions import *
 
-class LabTestCase(TestCase):
+class SectionTestCase(TestCase):
 
     def setUp(self):
         self.course1 = Course.objects.create(courseid="337", name="Systems Programming",credits="3")
         self.lab1 = Section.objects.create(course=self.course1, sectionid="902", type="Lab", schedule="R @ 11:00 - 12:50")
 
 #   CREATE COURSE TESTS
-    def test_labIdexists(self):
+    def test_SectionIdexists(self):
         self.assertEqual(createSection(self.course1, "902", "Lab", "T @ 11:00 - 12:50"),
                          "Section with that ID already exists")
 
-    def test_labCreated(self):
+    def test_SectionCreated(self):
         createSection(self.course1, "901", "Lab", "T @ 11:00 - 12:50")
         b =Section.objects.get(sectionid="901")
         self.assertEqual("Lab", b.type)
         self.assertEqual("T @ 11:00 - 12:50", b.schedule)
 
-    def test_nolabid(self):
+    def test_noSectionid(self):
         self.assertEqual(createSection(self.course1, "", "Lab", "R @ 11:00 - 12:50"),
                          "Please fill out all required entries")
 
-    def test_nolabename(self):
+    def test_noSectionType(self):
         self.assertEqual(createSection(self.course1, "901", "", "R @ 11:00 - 12:50"), "Please fill out all required entries")
 
-    def test_nolabschedule(self):
+    def test_noSectionschedule(self):
         self.assertEqual(createSection(self.course1, "901", "Lab", ""), "Please fill out all required entries")
 
 
 #   DELETE LAB TESTS
-    def test_deletenolabidentered(self):
+    def test_deletenosectionidentered(self):
         self.assertEqual(deleteSection(""), "Please enter a section ID")
 
-    def test_deletenolabexists(self):
+    def test_deletenosectionexists(self):
         self.assertEqual(deleteSection("901"), "Section with that ID does not exist")
 
-    def test_deletelab(self):
+    def test_deletesection(self):
         self.assertEqual(self.lab1.sectionid, "902")
         self.assertEqual(deleteSection("902"), "Section with ID 902 has been deleted")
